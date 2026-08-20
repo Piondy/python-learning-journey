ccfb06d5645d1747a7734ea941206ad075398e99
Pondy <piusgoodheadofficial@gmail.com>
2026-08-20 17:12:06 +0100

    Simple age calculator

---
+++ simple_age_calculator.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ")
+birth_year = int(input("What year were you born?: "))
+current_year = 2026
+age = current_year - birth_year
+print(f"Hello {name}!\nYou are approximately {age} years old.")
