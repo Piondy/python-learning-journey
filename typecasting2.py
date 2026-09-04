fce9f8b8a000c85c78dbead552f65436506d7b01
Piondy <piusgoodheadofficial@gmail.com>
2026-09-04 10:48:47 +0100

    Type casting challenge medium mode

---
+++ typecasting2.py
@@ -0,0 +1,8 @@
+banana = input("How many bananas? ")
+milk = input("How many milk? ")
+
+banana = int(banana)
+milk = float(milk)
+
+total = banana + milk
+print(f"You ordered {banana} bananas and {milk} cups of milk.\nTotal ingredients: {total}")
