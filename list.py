d26ec6a94ab6e9470929cccc072fc23494f0edf6
Piondy <piusgoodheadofficial@gmail.com>
2026-09-14 16:10:01 +0100

    List lesson

---
+++ list.py
@@ -0,0 +1,7 @@
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grape"]
+first_fruit = fruits[0]
+third_fruit = fruits[2]
+last_fruit = fruits[-1]
+print(first_fruit)
+print(third_fruit)
+print(last_fruit)
