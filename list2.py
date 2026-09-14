da55b425f5b992e75ba9244481db45e5a92df63d
Piondy <piusgoodheadofficial@gmail.com>
2026-09-14 16:19:28 +0100

    My list second challenge

---
+++ list2.py
@@ -0,0 +1,8 @@
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grape"]
+print(fruits[1:4])
+
+
+tasks = ["Research", "Analyze", "Code", "Test", "Deploy"]
+print(tasks[0])
+print(tasks[-1])
+print(tasks[1:4])
