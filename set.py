0126eb44cb414ee3156e8beccbe9f97b9badaa21
Piondy <piusgoodheadofficial@gmail.com>
2026-09-15 17:36:44 +0100

    Set lessons

---
+++ set.py
@@ -0,0 +1,21 @@
+languages = {"Python", "JavaScript", "Python", "C++"}
+print(languages)
+print(len(languages))
+
+ingredients = {"Cinnamon", "Cocoa", "Vanilla", "Nutmeg"}
+print("Cocoa" in ingredients)
+print("Chocolate" in ingredients)
+print("Chocolate" not in ingredients)
+
+python_team = {"Alice", "Bob", "Charlie"}
+javascript_team = {"Bob", "Charlie", "David"}
+union = python_team | javascript_team
+print(union)
+
+intersection = python_team & javascript_team
+print(intersection)
+
+difference = python_team - javascript_team
+print(difference)
+
+
