9fcd7382b991ba23690afe004891c92622300189
Piondy <piusgoodheadofficial@gmail.com>
2026-09-22 13:34:34 +0100

    Just a random challenge

---
+++ random_challenge.py
@@ -0,0 +1,12 @@
+class Name:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def greet(self):
+        return f"Hello, {self.full_name()}!"
+name1 = Name("John", "Doe")
+print(name1.greet())
